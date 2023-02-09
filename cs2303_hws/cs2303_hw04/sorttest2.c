6a69a56e9235555205558f2b09ec7b6eaa691b72
groundhog3 <ryanmechery@gmail.com>
2023-02-09 04:38:47 +0000

    hw04: done

--- cs2303_hws/cs2303_hw04/sorttest2.c
+++ cs2303_hws/cs2303_hw04/sorttest2.c
@@ -25,7 +25,7 @@
  * @return 0 if success, 1 if error (wrong number of arguments)
  */
 
-#define MAX_RANDOM (10)
+#define MAX_RANDOM (100)
 
 int main (int argc, const char* argv[]) {
   // Check that there are exactly two numbers
