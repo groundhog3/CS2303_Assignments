bf85e93ffe4028ba854f3300751f1e0ba5acadbd
groundhog3 <ryanmechery@gmail.com>
2023-02-21 04:46:17 +0000

    lab06: done

---
+++ cs2303_labs/cs2303_lab06/zipcode.c
@@ -0,0 +1,22 @@
+/** Program demonstrating parsing and linked lists. This program 
+ * supports zip code lookup for any town in Massachusetts
+ *
+ * @author Blake Nelson
+ *
+ */
+
+#include "zipfilter.h"
+
+/** program to find the zip code for any town in Massachusetts
+ *
+ * @param argc is the number of field on command line - should be 1
+ * @param argv is array of strings entered on command line - prog name
+ * @return 0 for success
+ */
+
+int main (int argc, char *argv[]) {
+  
+
+
+}
+
